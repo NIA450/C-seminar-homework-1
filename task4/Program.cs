f5248c89f155d6dea490ce4cfad114598762d917
Evgeniya Troshina <troshina450@gmail.com>
2023-09-04 15:31:43 +0300

    добавлен файл task4

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,5 +1,14 @@
 ﻿// напишите программу: деление одного числа на другое
 
-double numberA = 12;
-double numberB = 5;
-Console.WriteLine(numberA / numberB);
+// double numberA = 12; можно писать целые числа
+// double numberB = 5;
+// Console.WriteLine(numberA / numberB);
+
+// напишите программу, которая генерирует 2 случайных числа и выдает их сумму
+
+ int numberA = new Random().Next(1,10); //это генератор чисел
+ Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
