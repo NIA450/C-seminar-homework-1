edc4c016218e6e14bfc686ab819743a9bd6e5261
Evgeniya Troshina <troshina450@gmail.com>
2023-09-04 15:09:46 +0300

    добавила файлы task 3 4

---
+++ task3/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int numberA = 3;
+int numberB = 5;
+int numberC = 8;
+if (numberA > numberB)
+Console.WriteLine(numberA);
---
+++ task4/Program.cs
@@ -0,0 +1,5 @@
+﻿// напишите программу: деление одного числа на другое
+
+double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
