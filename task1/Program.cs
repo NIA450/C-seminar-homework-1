b4ade84731e51e6066cceab3d28b9e88f99ca9ee
Evgeniya Troshina <troshina450@gmail.com>
2023-09-04 12:26:29 +0300

    обновление

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,45 +1,5 @@
-﻿// Console.Write("Введите число a: ");
-// int a = int.Parse(Console.ReadLine()!);
-// Console.Write("Введите число b: ");
-// int b = int.Parse(Console.ReadLine()!);
-
-// if (a == (b * b))
-// {
-//     Console.WriteLine("True");
-// }
-// else
-// {
-//     Console.WriteLine("False");
-// }
-// Console.WriteLine("Введите номер дня недели: ");
-// string num = Console.ReadLine();
-// switch (num)
-// {
-//     case "1":
-//     Console.WriteLine("ПН: ");
-//     break;
-//     case "2":
-//     Console.WriteLine("ВТ: ");
-//     break;
-//     case "3":
-//     Console.WriteLine("СР: ");
-//     break;
-//     case "4":
-//     Console.WriteLine("ЧТ: ");
-//     break;
-//     case "5":
-//     Console.WriteLine("ПТ: ");
-//     break;
-//     case "6":
-//     Console.WriteLine("СБ: ");
-//     break;
-//     case "7":
-//     Console.WriteLine("ВС: ");
-//     break;
-//     default:
-//     Console.WriteLine("нет такого: ");
-//     break;
-// }
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
+//  какое число большее, а какое меньшее.
 
 var n1 = Decimal.Parse(Console.ReadLine());
 var n2 = Decimal.Parse(Console.ReadLine());
--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите положительное число: ");
+int num = int.Parse(Console.ReadLine());
+int i = 2;
+if (i > num) Console.WriteLine("Четных чисел от 1 до введеного числа нет");
+while (i <= num)
+{
+    if ((i % 2 == 0 && i == num) || (i % 2 == 0 && i + 1 == num))
+    {
+        Console.Write(i);
+    }
+    else if (i % 2 == 0) 
+    {
+        Console.Write(i + ",");
+    }
+    i++;
+}
