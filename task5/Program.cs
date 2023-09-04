7b64bb2ed389022869ffdd3bb1c244fec4ddb4e2
Evgeniya Troshina <troshina450@gmail.com>
2023-09-04 16:29:43 +0300

    добавлена задача четные числа

---
+++ task5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка)
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int even = 2;
+
+while (num >= even)
+{
+    Console.Write(even + " ");
+    even = even + 2;
+}
