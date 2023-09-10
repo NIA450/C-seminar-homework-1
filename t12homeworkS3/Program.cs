0c91a568c166b90088593e8afb1fd58ea1421720
Evgeniya Troshina <troshina450@gmail.com>
2023-09-10 19:54:26 +0300

    добавлена задача 29 но код не работает

---
+++ t12homeworkS3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+int [] arr={ 1, 5, 2, 4, 3, 6, 8, 7 };
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Lenght - 1; i++)
+    {
+        int minPosition = i;
+for (int j = i + 1; j < array.Lenght; j++)
+{
+    if(array[j] < array[minPosition]) minPosition = j;
+}
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
